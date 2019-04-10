2102980487ce2a53ee45bcdca67c4e9b667b58e7
Evan Morse <emorse@localhost.localdomain>
2019-04-10 17:42:42 -0400

    Import tlist data in main

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tlist_manager import TListManager
+import tlist_io
 import urwid 
 
-tlm = TListManager()
+tlm = TListManager(tlist_io.load_list_data())
 loop = urwid.MainLoop(tlm)
 try:
   loop.run()
