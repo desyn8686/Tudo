742a372883682701b0a3bd35d86ca134bcfa5775
Evan Morse <emorse@localhost.localdomain>
2019-04-10 17:38:16 -0400

    Clean up tlist_manager logic
    Pull all tlist_io management away from tlist_manager.
    From this point I'll be passing loaded data to the
    constructor for tlist_manager, and all save data will
    be pulled out of the manager before being passed to
    tlist_io.
    
    Removed several keybinds that will be moved/changed.

--- tlist_manager.py
+++ tlist_manager.py
@@ -1,4 +1,3 @@
-import tlist_io
 from column_viewport_focus_list import ColumnViewportFocusList
 from tlist import TList
 from urwid import MainLoop, WidgetWrap, WidgetPlaceholder, Overlay, Text, Filler, LineBox, ListBox, SimpleFocusListWalker, Columns
@@ -6,44 +5,28 @@ from urwid import AttrMap, AttrSpec
 
 class TListManager(WidgetWrap):
 
-  def __init__(self):
+  def __init__(self, tlist_data):
     self.tlists = ColumnViewportFocusList()
-
-    loaded_lists = tlist_io.load_list_data()
-    for data in loaded_lists:
-      tlist = TList(data)
-      self.tlists.append(tlist)
-
+    self.build_tlists(tlist_data)
     self.columns = Columns([], min_width = 32)
     super().__init__(self.columns)
 
-  def render(self, size, focus=False):
-    self.pack()
-    return super().render(size, focus)
+  def build_tlists(self, tlist_data):
+    for data in tlist_data:
+      tlist = TList(data)
+      self.tlists.append(tlist)
 
-  def new(self):
+  def build_new(self):
     self.tlists.insert(TList()) 
     self.pack()
 
-  def get_lists(self):
-    tlist_dicts = []
-    for tlist in self.tlists.contents:
-      tlist_dicts.append({'index': self.tlists.contents.index(tlist), 'name': tlist.name})
-    return tlist_dicts
-
-  def get_groups(self):
-    return('meow')
-
   def pack(self):
-    viewport_list = self.tlists.get_viewport()
+    viewport = self.tlists.get_viewport()
     if len(self.tlists.contents) > 0:
       self.columns.contents.clear()
       options = ('weight', 1, False)
-      for tlist in viewport_list:
-        if tlist == None:
-          column = ((Filler(Text('empty', align='center'))), options)
-        else:
-          column = (tlist, options)
+      for tlist in viewport:
+        column = (tlist, options)
         self.columns.contents.append(column)
       try:
         self.columns.focus_position = self.tlists.focus
@@ -56,6 +39,18 @@ class TListManager(WidgetWrap):
     self.tlists.focus += translation
     self.pack()
 
+  def delete_focus(self):
+    focus = self.columns.focus
+    tlist_io.delete_list(focus.id)
+    self.tlists.contents.remove(focus)
+    self.pack()
+
+  def pull_list_data(self):
+    tlist_data = []
+    for tlist in self.tlists.contents:
+      tlist_data.append(tlist.export())
+    return tlist_data
+
   def keypress(self, size, key):
     if self.columns.focus.is_editing:
       return super().keypress(size, key)
@@ -64,26 +59,8 @@ class TListManager(WidgetWrap):
         self.move_focus(1)
       elif key == 'H':
         self.move_focus(-1)
-      elif key == 'D':
-        return 'delete list', self
-      elif key == 'Q':
-        self.save_and_quit()
-        return 'quit'
       else: return super().keypress(size, key)
 
-  def delete_focus(self):
-    focus = self.columns.focus
-    tlist_io.delete_list(focus.id)
-    self.tlists.contents.remove(focus)
+  def render(self, size, focus=False):
     self.pack()
-
-  def save_and_quit(self):
-    for tlist in self.tlists.contents:
-      tlist_io.save_list(tlist.export())
-    
-
-if __name__ == '__main__':
-  manager = TListManager()
-  manager.tlists.focus = 0
-  loop = MainLoop(manager)
-  loop.run()
+    return super().render(size, focus)
