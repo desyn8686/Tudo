f13f97b8849984f36fbf90e23e2699f97a7de184
Evan Morse <e.morse8686@gmail.com>
2019-04-12 14:32:35 -0400

    Add ability to add caption to change_prompt
    Important for use with group_foot

--- change_prompt.py
+++ change_prompt.py
@@ -3,9 +3,10 @@ import urwid
 class ChangePrompt(urwid.WidgetWrap):
   
   signals = ['close']
-  def __init__(self, text):
+  def __init__(self, text, caption=None):
     self.reponse = ''
     self.edit = urwid.Edit(edit_text=text, align='center')
+    if caption: self.edit.set_caption(caption)
     self.map = urwid.AttrMap(self.edit, urwid.AttrSpec('h231', 'h160'))
     self.fill = urwid.Filler(self.map)
     super().__init__(self.fill)
