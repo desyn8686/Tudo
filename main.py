5ce49417e1e0aaa2c1e6f96784d9cf03685938c2
Evan Morse <e.morse8686@gmail.com>
2019-04-12 14:33:49 -0400

    Fix save on interrupt

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from tlist_manager import TListManager
 import tlist_io
 import urwid 
+import signal
+import os
+
+#class SuspendProcess(Exception):
+#  pass
+
+#def suspend(signum, frame):
+#  raise SuspendProcess
+
+#signal.signal(signal.SIGTSTP, suspend)
 
 tlm = TListManager(tlist_io.load_list_data())
 loop = urwid.MainLoop(tlm, pop_ups=True)
-try:
-  loop.run()
-except KeyboardInterrupt:
-  tlm.save_and_quit()  
-  print('Goodbye!')
+running = True
+while running:
+  try:
+    loop.run()
+  except KeyboardInterrupt:
+    data = tlm.pull_list_data()  
+    tlist_io.save_all(data)
+    running = False
+print('Goodbye!')
